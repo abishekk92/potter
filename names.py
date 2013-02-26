bf8dcb54b93b14589dbd697319b90da428af9fc5
Abishek <bhat.abishek@gmail.com>
2013-02-26 18:38:46 +0530

    Names Filter WIP

--- names.py
+++ names.py
@@ -4,4 +4,5 @@ book_1 = open(os.path.join("dataset","book_1.txt"),'r').read()
 sentences=nltk.sent_tokenize(book_1)
 sentences=[nltk.word_tokenize(sent) for sent in sentences]
 sentences=[nltk.pos_tag(sent) for sent in sentences]
-print sentences
+chunked=[nltk.ne_chunk(sent) for sent in sentences]
+for chunk in chunked`
