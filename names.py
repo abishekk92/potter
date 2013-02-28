badcc45ce09ab1e5c3ca22ed9f637f39c36f34f0
Abishek <bhat.abishek@gmail.com>
2013-02-28 13:34:33 +0530

    Implementing the chunker

--- names.py
+++ names.py
@@ -5,4 +5,10 @@ sentences=nltk.sent_tokenize(book_1)
 sentences=[nltk.word_tokenize(sent) for sent in sentences]
 sentences=[nltk.pos_tag(sent) for sent in sentences]
 chunked=[nltk.ne_chunk(sent) for sent in sentences]
-for chunk in chunked`
+for chunk in chunked:
+	if hasattr(chunk,'node'):		
+		if chunk.node == 'PERSON':
+			name=' '.join(leave[0] for leave in chunk.leaves())
+			print name
+	else:
+		print "Nope"
